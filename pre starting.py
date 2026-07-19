3f22570776a4fcb2ea5c10cf2119f51952c6dba3
Nikhil Sonawane <hinikhilbr.o40@gmail.com>
2026-07-19 18:44:45 +0530

    Add files via upload

---
+++ pre starting.py
@@ -0,0 +1,13 @@
+string="This is String"
+print(string)
+
+no= 25
+print(no)
+
+float= 25.5
+print(float)
+
+placement_done=False
+print(placement_done)
+
+print(f"String is: {string}, Number is: {no}, Float is: {float}, Placement done: {placement_done}")

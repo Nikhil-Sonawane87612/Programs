5492b0d4d440377c24f53fe5949ebd2ad2f4fa16
Nikhil-Sonawane87612 <hinikhilbr.o40@gmail.com>
2026-07-20 09:06:11 +0530

    DAY 1: Added Commute Loop Script

---
+++ commute_loop.py
@@ -0,0 +1,7 @@
+distance_remaining=100
+print("Commute Started..... Tracking Distance....")
+while distance_remaining > 0:
+    print(f"Distance Left {distance_remaining}km. Keep Learning")
+    distance_remaining-=20
+
+print("Distance Reached! Time To Code")

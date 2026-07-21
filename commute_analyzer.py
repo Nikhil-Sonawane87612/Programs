b6dbf1927b9c2b9932da2653f5923259ee1997c0
Nikhil-Sonawane87612 <hinikhilbr.o40@gmail.com>
2026-07-21 23:32:45 +0530

    Day 1: Added Commute List Analyser Script

---
+++ commute_analyzer.py
@@ -0,0 +1,9 @@
+weekly_commute=[5.0, 4.5, 5.2, 4.8, 5.0]
+total_hours=sum(weekly_commute)
+avg_hours= total_hours / len(weekly_commute)
+day_num=1
+for time in weekly_commute:
+    print(f"Day : {day_num} : {time} hours ")
+    day_num +=1
+print(f"Total Travel this Week : {total_hours}")
+print(f"Average Travel this Week : {avg_hours}")
